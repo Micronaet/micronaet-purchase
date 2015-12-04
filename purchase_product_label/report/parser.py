f97202fac9431ad2e95abd0aba573fa7d24e4112
cherrygirl <anna@micronaet.it>
2015-12-04 09:03:01 +0100

    Create label report

--- purchase_product_label/report/parser.py
+++ purchase_product_label/report/parser.py
@@ -28,6 +28,14 @@ class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.localcontext.update({
+            'get_objects': self.get_objects,
             # TODO
             
         })
+        
+
+
+
+
+
+
