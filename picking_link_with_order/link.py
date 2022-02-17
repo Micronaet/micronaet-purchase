825b4c6ded88dbe50a07508978842ce00409200d
nicothebrush <nicola.riolini@gmail.com>
2022-02-17 12:02:12 +0100

    pep 8 normalitazion

--- picking_link_with_order/link.py
+++ picking_link_with_order/link.py
@@ -31,31 +31,32 @@ from openerp import SUPERUSER_ID, api
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
 _logger = logging.getLogger(__name__)
 
+
 class PurchaseOrder(orm.Model):
     """ Model name: Purchase Order
     """
-    
+
     _inherit = 'purchase.order'
-    
+
     # -------------------------------------------------------------------------
     # Button event:
     # -------------------------------------------------------------------------
     def link_purchase_line_with_sale(self, cr, uid, ids, context=None):
-        ''' Link all product with order open product 
-        '''
+        """ Link all product with order open product
+        """
         # Pool used:
         sol_pool = self.pool.get('sale.order.line')
-        
+
         order = self.browse(cr, uid, ids, context=context)[0]
-        
+
         # ---------------------------------------------------------------------
         # Track product total received in picking:
         # ---------------------------------------------------------------------
@@ -66,57 +67,57 @@ class PurchaseOrder(orm.Model):
             default_code = product.default_code
             if default_code not in res:
                 res[default_code] = [
-                    0, # Total received
-                    0, # Total open order
-                    [], # SOL lines order
-                    product, # Product
+                    0,  # Total received
+                    0,  # Total open order
+                    [],  # SOL lines order
+                    product,  # Product
                     ]
                 product_ids.append(product.id)
 
             res[default_code][0] += line.product_qty
-            
+
         # ---------------------------------------------------------------------
         # Link sale order information:
         # ---------------------------------------------------------------------
         sol_ids = sol_pool.search(cr, uid, [
             # Only this product:
             ('product_id', 'in', product_ids),
-            
+
             # In correct state:
             ('order_id.state', 'not in', ('cancel', 'draft', 'sent', 'done')),
-            
+
             # Not marked as closed:
             ('mx_closed', '=', False),
             ('order_id.mx_closed', '=', False),
             ], context=context)
         for sol in sol_pool.browse(cr, uid, sol_ids, context=context):
             product = sol.product_id
-            default_code = product.default_code            
+            default_code = product.default_code
             remain = sol.product_uom_qty - sol.delivered_qty
             if remain <= 0.0:
                 continue
 
             res[default_code][1] += remain
             res[default_code][2].append(sol)
-            
+
         # ---------------------------------------------------------------------
         # Generate file XLSX
-        # ---------------------------------------------------------------------            
+        # ---------------------------------------------------------------------
         excel_pool = self.pool.get('excel.writer')
         ws_name = 'Confronto ordini'
         excel_pool.create_worksheet(ws_name)
 
         # ---------------------------------------------------------------------
         # Formats
-        # ---------------------------------------------------------------------            
-        excel_pool.set_format()        
+        # ---------------------------------------------------------------------
+        excel_pool.set_format()
         f_header = excel_pool.get_format('header')
         f_text = excel_pool.get_format('text')
         f_number = excel_pool.get_format('number')
 
         # ---------------------------------------------------------------------
         # Setup col dimension:
-        # ---------------------------------------------------------------------            
+        # ---------------------------------------------------------------------
         excel_pool.column_width(ws_name, [
             15, 30, 20, 10, 10, 1, 15, 12, 30, 10])
         row = 0
@@ -131,7 +132,7 @@ class PurchaseOrder(orm.Model):
             'SCADENZA',
             'CLIENTE',
             'Q.',
-            ], default_format=f_header)    
+            ], default_format=f_header)
 
         # ---------------------------------------------------------------------
         # Prepare price last buy check
@@ -139,7 +140,7 @@ class PurchaseOrder(orm.Model):
         for default_code, data in res.iteritems():
             row += 1
             received, order, sol, product = data
-            
+
             # -----------------------------------------------------------------
             # Write data row::
             # -----------------------------------------------------------------
@@ -149,48 +150,48 @@ class PurchaseOrder(orm.Model):
                 product.name,
                 product.colour,
                 (received, f_number),
-                (order, f_number),                
+                (order, f_number),
                 '',
                 '',
                 '',
                 '',
                 ('', f_number),
                 ], default_format=f_text)
-            
+
             # Order detail:
             if sol:
-                row -= 1 # for write in the same line
-                   
+                row -= 1  # for write in the same line
+
             for line in sol:
                 row += 1
                 excel_pool.write_xls_line(ws_name, row, [
                     line.order_id.name,
                     line.date_deadline or line.order_id.date_deadline or '',
-                    line.order_id.partner_id.name, 
-                    (line.product_uom_qty - line.delivered_qty, 
+                    line.order_id.partner_id.name,
+                    (line.product_uom_qty - line.delivered_qty,
                         f_number)
                     ], default_format=f_text, col=6)
         return excel_pool.return_attachment(cr, uid, 'Confronto acquisti')
 
+
 class StockPicking(orm.Model):
     """ Model name: StockPicking
     """
-    
+
     _inherit = 'stock.picking'
 
     # -------------------------------------------------------------------------
     # Button event:
     # -------------------------------------------------------------------------
     def link_purchase_line_with_sale(self, cr, uid, ids, context=None):
-        ''' Link all product with order open product 
-        '''
-        import pdb; pdb.set_trace()
+        """ Link all product with order open product
+        """
         # Pool used:
         sol_pool = self.pool.get('sale.order.line')
         attachment_pool = self.pool.get('ir.attachment')
-        
+
         picking = self.browse(cr, uid, ids, context=context)[0]
-        
+
         # ---------------------------------------------------------------------
         # Track product total received in picking:
         # ---------------------------------------------------------------------
@@ -201,42 +202,42 @@ class StockPicking(orm.Model):
             default_code = product.default_code
             if default_code not in res:
                 res[default_code] = [
-                    0, # Total received
-                    0, # Total open order
-                    [], # SOL lines order
-                    product, # Product
+                    0,  # Total received
+                    0,  # Total open order
+                    [],  # SOL lines order
+                    product,  # Product
                     ]
                 product_ids.append(product.id)
 
             res[default_code][0] += line.product_uom_qty
-            
+
         # ---------------------------------------------------------------------
         # Link sale order information:
         # ---------------------------------------------------------------------
         sol_ids = sol_pool.search(cr, uid, [
             # Only this product:
             ('product_id', 'in', product_ids),
-            
+
             # In correct state:
             ('order_id.state', 'not in', ('cancel', 'draft', 'sent', 'done')),
-            
+
             # Not marked as closed:
             ('mx_closed', '=', False),
             ('order_id.mx_closed', '=', False),
             ], context=context)
         for sol in sol_pool.browse(cr, uid, sol_ids, context=context):
             product = sol.product_id
-            default_code = product.default_code            
+            default_code = product.default_code
             remain = sol.product_uom_qty - sol.delivered_qty
             if remain <= 0.0:
                 continue
 
             res[default_code][1] += remain
             res[default_code][2].append(sol)
-            
+
         # ---------------------------------------------------------------------
         # Generate file XLSX
-        # ---------------------------------------------------------------------            
+        # ---------------------------------------------------------------------
         filename = '/tmp/check_purchase.xlsx'
         _logger.info('Extract: %s' % filename)
         WB = xlsxwriter.Workbook(filename)
@@ -244,18 +245,18 @@ class StockPicking(orm.Model):
 
         # ---------------------------------------------------------------------
         # Formats
-        # ---------------------------------------------------------------------            
+        # ---------------------------------------------------------------------
         num_format = '#,##0'
         format_header = WB.add_format({
-            'bold': True, 
+            'bold': True,
             'font_color': 'black',
-            'font_name': 'Courier 10 pitch', # 'Arial'
+            'font_name': 'Courier 10 pitch',  # 'Arial'
             'font_size': 9,
             'align': 'center',
             'valign': 'vcenter',
-            'bg_color': '#cfcfcf', # gray
+            'bg_color': '#cfcfcf',  # gray
             'border': 1,
-            #'text_wrap': True,
+            # 'text_wrap': True,
             })
         format_text = WB.add_format({
             'font_color': 'black',
@@ -274,7 +275,7 @@ class StockPicking(orm.Model):
 
         # ---------------------------------------------------------------------
         # Setup col dimension:
-        # ---------------------------------------------------------------------            
+        # ---------------------------------------------------------------------
         WS.set_column('A:A', 15)
         WS.set_column('B:B', 30)
         WS.set_column('C:C', 20)
@@ -294,7 +295,7 @@ class StockPicking(orm.Model):
 
         WS.write(counter, 3, 'VOLUME IMB.', format_header)
         WS.write(counter, 4, 'PZ X SCAT.', format_header)
-        
+
         WS.write(counter, 5, 'ARRIVATO', format_header)
         WS.write(counter, 6, 'ORDINATO', format_header)
         WS.write(counter, 7, '', format_header)
@@ -309,7 +310,7 @@ class StockPicking(orm.Model):
         for default_code, data in res.iteritems():
             counter += 1
             received, order, sol, product = data
-            
+
             # -----------------------------------------------------------------
             # Write data row::
             # -----------------------------------------------------------------
@@ -320,31 +321,31 @@ class StockPicking(orm.Model):
 
             WS.write(counter, 3, '', format_text)
             WS.write(counter, 4, product.q_x_pack, format_text)
-            
+
             WS.write(counter, 5, received, format_number)
             WS.write(counter, 6, order, format_number)
-            
+
             # Order detail:
-            if sol: 
-                counter -= 1 # for write in the same line
+            if sol:
+                counter -= 1  # for write in the same line
             else:
                 WS.write(counter, 8, '', format_text)
                 WS.write(counter, 9, '', format_text)
                 WS.write(counter, 10, '', format_text)
                 WS.write(counter, 11, '', format_number)
-                   
+
             for line in sol:
                 counter += 1
                 WS.write(counter, 8, line.order_id.name, format_text)
                 WS.write(
-                    counter, 9, 
-                    line.date_deadline or line.order_id.date_deadline or '', 
+                    counter, 9,
+                    line.date_deadline or line.order_id.date_deadline or '',
                     format_text)
-                
-                WS.write(counter, 10, line.order_id.partner_id.name, 
+
+                WS.write(counter, 10, line.order_id.partner_id.name,
                     format_text)
-                WS.write(counter, 11, 
-                    line.product_uom_qty - line.delivered_qty, 
+                WS.write(counter, 11,
+                    line.product_uom_qty - line.delivered_qty,
                     format_number)
         WB.close()
 
@@ -361,11 +362,10 @@ class StockPicking(orm.Model):
             'res_model': 'res.partner',
             'res_id': 1,
             }, context=context)
-            
+
         return {
-            'type' : 'ir.actions.act_url',
+            'type': 'ir.actions.act_url',
             'url': '/web/binary/saveas?model=ir.attachment&field=datas&'
-                'filename_field=datas_fname&id=%s' % attachment_id,
+                   'filename_field=datas_fname&id=%s' % attachment_id,
             'target': 'self',
             }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
